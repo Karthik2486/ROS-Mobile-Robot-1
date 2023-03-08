46497bce684ccbc1d9a1278d68e007367c456e62
Karthik2486 <karthikjudo700@gmail.com>
2023-03-08 13:40:04 +0530

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/karthik/kb_mobile_robot1_ws/src
+# Build directory: /home/karthik/kb_mobile_robot1_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("kb_car2_description")
---
+++ build/kb_car2_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/karthik/kb_mobile_robot1_ws/src/kb_car2_description
+# Build directory: /home/karthik/kb_mobile_robot1_ws/build/kb_car2_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
